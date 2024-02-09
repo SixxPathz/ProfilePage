25dee0f7ec398d651d18a201d3cc3438938baf27
Sandile Dhlamini <dsandile58@gmail.com>
2024-02-09 09:13:23 +0200

    upload project

---
+++ BitcubeProject/BitcubeProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BitcubeProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BitcubeProject/BitcubeProject/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace BitcubeProject
+
+    
+
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+       
+    }
+
+        private void EditProfile_Clicked(object sender, EventArgs e)
+        {
+           
+            string websiteUrl = "https://sandile.netlify.app";
+
+        
+            Launcher.OpenAsync(new Uri(websiteUrl));
+        }
+
+     
+    }
+
+}
